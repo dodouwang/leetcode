7e738479d819acb251e043f1b480f1d34871192a
dodouwang <37562495+dodouwang@users.noreply.github.com>
2018-04-21 19:11:21 +0800

    update 50.powx-n.cpp

---
+++ 50.powx-n.cpp
@@ -0,0 +1,82 @@
+/*
+ * [50] Pow(x, n)
+ *
+ * https://leetcode.com/problems/powx-n/description/
+ *
+ * algorithms
+ * Medium (26.06%)
+ * Total Accepted:    209.5K
+ * Total Submissions: 803.9K
+ * Testcase Example:  '2.00000\n10'
+ *
+ * Implement pow(x, n), which calculates x raised to the power n (xn).
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: 2.00000, 10
+ * Output: 1024.00000
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: 2.10000, 3
+ * Output: 9.26100
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: 2.00000, -2
+ * Output: 0.25000
+ * Explanation: 2-2 = 1/22 = 1/4 = 0.25
+ * 
+ * 
+ * Note:
+ * 
+ * 
+ * -100.0 < x < 100.0
+ * n is a 32-bit signed integer, within the range [−231, 231 − 1]
+ * 
+ * 
+ */
+class Solution {
+public:
+    double myPow(double x, int n) {
+        double r = 1;
+        double zheng1 = 1;
+        double fu1 = -1;
+        double z = 0;
+        if (n == 0) {
+            return 1;
+        }
+
+        if (x == z) {
+            return 0;
+        } else if (x == zheng1) {
+            return r;
+        } else if (x == fu1) {
+            if (n % 2) {
+                return fu1;
+            } else {
+                return zheng1;
+            }
+        }
+
+
+
+        if (n > 0) {
+            while (n-- && r != z) {
+                r *= x;
+            }
+            return r;
+        } else {
+            while (n++ && r != z) {
+                r /= x;
+            }
+            return r;
+            
+        }
+    }
+};

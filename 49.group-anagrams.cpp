765f56d481be647cb8211948c07c55d0948f130b
dodouwang <37562495+dodouwang@users.noreply.github.com>
2018-04-21 18:14:41 +0800

    update 49.group-anagrams.cpp

--- 49.group-anagrams.cpp
+++ 49.group-anagrams.cpp
@@ -58,7 +58,7 @@ public:
         return t;
     } 
 
-    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+    vector<vector<string>> groupAnagrams1(vector<string>& strs) {
         unordered_map<string, multiset<string>> mp;
         for (string s : strs) {
             string t = s; 
@@ -73,7 +73,7 @@ public:
             return anagrams;
     }
     
-    vector<vector<string>> groupAnagrams1(vector<string>& strs) {
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
         vector<vector<string>> r;
         map<vector<int>, vector<string>> m;
         vector<int> v(26, 0);

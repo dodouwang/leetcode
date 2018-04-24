eb1720754335f4d46f61f439e6102deba8accf67
dodouwang <37562495+dodouwang@users.noreply.github.com>
2018-04-24 11:10:38 +0800

    update 63.unique-paths-ii.cpp

---
+++ 63.unique-paths-ii.cpp
@@ -0,0 +1,72 @@
+/*
+ * [63] Unique Paths II
+ *
+ * https://leetcode.com/problems/unique-paths-ii/description/
+ *
+ * algorithms
+ * Medium (32.21%)
+ * Total Accepted:    134.7K
+ * Total Submissions: 418.1K
+ * Testcase Example:  '[[0,0,0],[0,1,0],[0,0,0]]'
+ *
+ * A robot is located at the top-left corner of a m x n grid (marked 'Start' in
+ * the diagram below).
+ * 
+ * The robot can only move either down or right at any point in time. The robot
+ * is trying to reach the bottom-right corner of the grid (marked 'Finish' in
+ * the diagram below).
+ * 
+ * Now consider if some obstacles are added to the grids. How many unique paths
+ * would there be?
+ * 
+ * 
+ * 
+ * An obstacle and empty space is marked as 1 and 0 respectively in the grid.
+ * 
+ * Note: m and n will be at most 100.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input:
+ * [
+ * [0,0,0],
+ * [0,1,0],
+ * [0,0,0]
+ * ]
+ * Output: 2
+ * Explanation:
+ * There is one obstacle in the middle of the 3x3 grid above.
+ * There are two ways to reach the bottom-right corner:
+ * 1. Right -> Right -> Down -> Down
+ * 2. Down -> Down -> Right -> Right
+ * 
+ * 
+ */
+class Solution {
+public:
+    // 直接计算，时间O(m*n)，优化空间O(min(m,n))，空间可优化，考虑(n+m)! / m! / n!，O(1)即可
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int n = obstacleGrid.size();
+        if (n == 0) {
+            return 1;
+        }
+        int m = obstacleGrid[0].size();
+
+        vector<int> v(m, 0);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (obstacleGrid[i][j] == 1) {
+                    v[j] = 0;
+                } else {
+                    if (i == 0 && j == 0) {
+                        v[j] = 1;
+                    } else {
+                        v[j] += v[j-1];
+                    }
+                }
+            }
+        }
+        return v[m-1];
+    }
+};

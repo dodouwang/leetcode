d353df32e40982e52bb05d5c198556c76fd21426
dodouwang <37562495+dodouwang@users.noreply.github.com>
2018-04-25 14:46:05 +0800

    update 77.combinations.cpp

--- 77.combinations.cpp
+++ 77.combinations.cpp
@@ -30,7 +30,52 @@
  */
 class Solution {
 public:
+    // 优化一下，记录下自顶向下的结果备查
+    map<pair<int,int>, vector<vector<int> > > m;
     vector<vector<int>> combine(int n, int k) {
+        vector<int> r_in;
+        vector<vector<int>> r;
+        pair<int,int> p;
+        if (k == 0) {
+            r.push_back(r_in);
+            return r;
+        }
+
+        if (n < k) {
+            return r;
+        } else if (n == k) {
+            for (int i = 0; i < n; ++i) {
+                r_in.push_back(i+1);
+            }
+            r.push_back(r_in);
+            return r;
+        } else {
+            auto x = this->m.find(make_pair(n-1,k));
+            if (x!= this->m.end()) {
+                r = x->second;
+            } else {
+                r = this->combine(n-1, k);
+                this->m[make_pair(n-1,k)] = r;
+            }
+
+            vector<vector<int>> tmp;
+            x = this->m.find(make_pair(n-1,k-1));
+            if (x!= this->m.end()) {
+                tmp = x->second;
+            } else {
+                tmp = this->combine(n-1, k-1);
+                this->m[make_pair(n-1,k-1)] = tmp;
+            }
+
+            for (auto &a :tmp) {
+                a.push_back(n);
+            }
+            r.insert(r.end(), tmp.begin(), tmp.end());
+            return r;
+        }
+    }    
+    // 递归方式，f(n,k) = f(n-1,k) + f(n-1,k-1)[n]
+    vector<vector<int>> combine1(int n, int k) {
         vector<int> r_in;
         vector<vector<int>> r;
         if (k == 0) {

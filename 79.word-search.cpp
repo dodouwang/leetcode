908a285812b458fc9e9b51d70d2c0c6ecba4dcf5
dodouwang <37562495+dodouwang@users.noreply.github.com>
2018-04-27 07:01:02 +0800

    update 79.word-search.cpp

--- 79.word-search.cpp
+++ 79.word-search.cpp
@@ -38,11 +38,69 @@ public:
 };
 class Solution {
 public:
+    // 前一种超时方案的问题在于：BFS割裂了单次搜寻的过程。
+    // 每走一步记录当前的所有可行状态，每个状态里都要记录当前尾部和所有用过的位置。
+    // 状态无法复用，复制占用空间和时间都多。
+    // 现改用DFS，每次都尝试走到底
+    bool exist(vector<vector<char>>& board, string word) {
+        size_t n = board.size();
+        if (n == 0) {
+            return false;
+        }
+        size_t m = board[0].size();
+        if (m == 0) {
+            return false;
+        }
+        size_t l = word.length();
+        if (l == 0 || l > n*m) {
+            return false;
+        }
+        
+        set<pair<int, int>> visited;
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < m; ++j) {
+                if (this->can_go(i, j, word, 0, visited, board)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    bool can_go(int i, int j, string & word, int k, set<pair<int, int>> &visited, vector<vector<char>>& board) {
+        // 调用时需保证ijk合法，若失败，要自行保持visited状态不变
+        if (board[i][j] != word[k]) {
+            return false;
+        }
+        pair<int, int> p = make_pair(i,j);
+        if (visited.find(p) != visited.end()) {
+            return false;
+        }
+        
+        if (++k == word.length()) {
+            return true;
+        }
+        visited.insert(p);
+        int change[4][2] = {{-1,0},{1,0},{0,-1},{0,1}};
+        for (int z = 0; z < 4; ++z) {
+            int newi = i + change[z][0], newj = j + change[z][1];
+            if (newi < 0 || newj < 0 || newi >= board.size() || newj >= board[0].size()) {
+                continue;
+            }
+            if (this->can_go(newi, newj, word, k, visited, board)) {
+                return true;
+            }
+        }
+        visited.erase(p);
+        return false;
+    }
+    
     // 采用递推而非递归
     // f(n) 要带有目前的所有可行性vector，每个元素一是要有当前位置，二是要有已用过的位置set
     // f(n+1) 要遍历fn的vector，看每个元素的周边四元素若一来没用过，二来是n+1的字符，则可以加入新字符后扩充vector，最后删掉旧元素。
     // 要先做一遍找出头来
-    bool exist(vector<vector<char>>& board, string word) {
+    // 最后超时
+    bool exist1(vector<vector<char>>& board, string word) {
         size_t n = board.size();
         if (n == 0) {
             return false;
@@ -70,6 +128,7 @@ public:
                 }
             }
         }
+
         for (int i = 1; i < l; ++i) {
             int vl = v.size();
             if (vl == 0) {
@@ -85,7 +144,7 @@ public:
         return v.size() != 0;
     }
     void handle_v_with_node(vector<Node> &v, Node &now, char c, vector<vector<char>>& board) {
-        int change[4][2] = {{-1,1},{1,1},{-1,-1},{1,-1}};
+        int change[4][2] = {{-1,0},{1,0},{0,-1},{0,1}};
         for (int i = 0; i < 4; ++i) {
             pair<int, int> np = make_pair(now.p.first+change[i][0],now.p.second+change[i][1]);
             if (np.first < 0 || np.second < 0 || np.first >= board.size() || np.second >= board[0].size()) {

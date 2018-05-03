47a850cc36e52f107e3d5faf888337297d008231
dodouwang <37562495+dodouwang@users.noreply.github.com>
2018-05-03 15:43:27 +0800

    update 94.binary-tree-inorder-traversal.cpp

--- 94.binary-tree-inorder-traversal.cpp
+++ 94.binary-tree-inorder-traversal.cpp
@@ -37,7 +37,76 @@
  */
 class Solution {
 public:
+//    vector<int> inorderTraversal2(TreeNode *root) {
+//        vector<int> vector;
+//        stack<TreeNode *> stack;
+//        TreeNode *pCurrent = root;
+//
+//        while(!stack.empty() || pCurrent) {
+//            if(pCurrent) {
+//                stack.push(pCurrent);
+//                pCurrent = pCurrent->left;
+//            } else {
+//                TreeNode *pNode = stack.top();
+//                vector.push_back(pNode->val);
+//                stack.pop();
+//                pCurrent = pNode->right;
+//            }
+//        }
+//        return vector;
+//    }
+//    // 换个思路，有左就一直往里放，没左就输出自己，pop自己，然后放右
+//
+//    vector<int> inorderTraversal2(TreeNode *root) {
+//        vector<int> vector;
+//        stack<TreeNode *> stack;
+//        unordered_map<TreeNode *, bool> map;
+//        
+//        stack.push(root);
+//
+//        while(!stack.empty()) {
+//            TreeNode *tmp = stack.top();
+//            if (map[tmp]) {
+//                vector.push_back(tmp->val);
+//                stack.pop();
+//            } else if (tmp->left) {
+//                map[tmp] = true;
+//                stack.push(tmp->left);
+//            } else {
+//                map[tmp] = true;
+//                if (tmp->right) {
+//                    stack.push(tmp->right);
+//                }
+//            }
+//        }
+//        return vector;
+//    }
+
+    // 换个思路，不更改节点，用map来记录，每个节点都是两进两出，比较繁琐
     vector<int> inorderTraversal(TreeNode* root) {
+        stack<TreeNode *> s;
+        vector<int> r;
+        unordered_map<TreeNode *, bool> map;
+        
+        if (root == NULL) return r;
+        s.push(root);
+        while (!s.empty()) {
+            TreeNode *i = s.top();
+            s.pop();
+            if (map[i]) {
+                r.push_back(i->val);
+                continue;
+            }
+            if (i->right) s.push(i->right);
+            map[i] = true;
+            s.push(i);
+            if (i->left) s.push(i->left);
+        }
+        return r;
+    }
+
+    // 简单思维，先放root进去，然后只要不空就pop出来，放右中左进去，放中时做好标记。空就打印当前value
+    vector<int> inorderTraversal1(TreeNode* root) {
         stack<TreeNode *> s;
         vector<int> r;
         if (root == NULL) return r;

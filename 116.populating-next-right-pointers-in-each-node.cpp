cdc8260033d1fb5fbf3fc4effaaf6989e4a1ce8c
dodouwang <37562495+dodouwang@users.noreply.github.com>
2018-05-10 10:43:29 +0800

    update 116.populating-next-right-pointers-in-each-node.cpp

--- 116.populating-next-right-pointers-in-each-node.cpp
+++ 116.populating-next-right-pointers-in-each-node.cpp
@@ -67,7 +67,25 @@
  */
 class Solution {
 public:
+    // 利用了上一层的next，来进行逐层扫描
     void connect(TreeLinkNode *root) {
+        if(!root)
+            return;
+        while(root -> left)
+        {
+            TreeLinkNode *p = root;
+            while(p)
+            {
+                p -> left -> next = p -> right;
+                if(p -> next)
+                    p -> right -> next = p -> next -> left;
+                p = p -> next;
+            }
+            root = root -> left;
+        }
+    }
+
+    void connect1(TreeLinkNode *root) {
         if (!root) {
             return;
         }

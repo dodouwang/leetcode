650c5dc034ca3395a95d736cfeae88c35a8f870a
dodouwang <37562495+dodouwang@users.noreply.github.com>
2018-05-28 12:07:02 +0800

    update 138.copy-list-with-random-pointer.cpp

---
+++ 138.copy-list-with-random-pointer.cpp
@@ -0,0 +1,59 @@
+/*
+ * [138] Copy List with Random Pointer
+ *
+ * https://leetcode.com/problems/copy-list-with-random-pointer/description/
+ *
+ * algorithms
+ * Medium (25.80%)
+ * Total Accepted:    157.6K
+ * Total Submissions: 611.1K
+ * Testcase Example:  '{}'
+ *
+ * 
+ * A linked list is given such that each node contains an additional random
+ * pointer which could point to any node in the list or null.
+ * 
+ * 
+ * 
+ * Return a deep copy of the list.
+ * 
+ */
+/**
+ * Definition for singly-linked list with a random pointer.
+ * struct RandomListNode {
+ *     int label;
+ *     RandomListNode *next, *random;
+ *     RandomListNode(int x) : label(x), next(NULL), random(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    RandomListNode *copyRandomList(RandomListNode *head) {
+        map<RandomListNode*, RandomListNode*> mp;
+        RandomListNode *t = head;
+        RandomListNode *r_tmp = new RandomListNode(0);
+        RandomListNode *r_pre = r_tmp;
+        while (t) {
+            RandomListNode *r = new RandomListNode(t->label);
+            mp[t] = r;
+            r_pre->next = r;
+            r_pre = r;
+            t = t->next;
+        }
+
+        t = head;
+        while (t) {
+            if (t->random) {
+                mp[t]->random = mp[t->random];
+            }
+            t = t->next;
+        }
+
+        return r_tmp->next;
+    }
+};
+static int x=[](){
+    std::ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    return 0;
+}();

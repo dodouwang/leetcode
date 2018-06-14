799182ad1e0234d063de25ed858cffb834d530a0
dodouwang <37562495+dodouwang@users.noreply.github.com>
2018-06-14 13:02:43 +0800

    update 147.insertion-sort-list.cpp

---
+++ 147.insertion-sort-list.cpp
@@ -0,0 +1,103 @@
+/*
+ * [147] Insertion Sort List
+ *
+ * https://leetcode.com/problems/insertion-sort-list/description/
+ *
+ * algorithms
+ * Medium (34.11%)
+ * Total Accepted:    121.8K
+ * Total Submissions: 356.2K
+ * Testcase Example:  '[4,2,1,3]'
+ *
+ * Sort a linked list using insertion sort.
+ * 
+ * 
+ * 
+ * 
+ * 
+ * A graphical example of insertion sort. The partial sorted list (black)
+ * initially contains only the first element in the list.
+ * With each iteration one element (red) is removed from the input data and
+ * inserted in-place into the sorted list
+ * 
+ * 
+ * 
+ * 
+ * 
+ * Algorithm of Insertion Sort:
+ * 
+ * 
+ * Insertion sort iterates, consuming one input element each repetition, and
+ * growing a sorted output list.
+ * At each iteration, insertion sort removes one element from the input data,
+ * finds the location it belongs within the sorted list, and inserts it
+ * there.
+ * It repeats until no input elements remain.
+ * 
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: 4->2->1->3
+ * Output: 1->2->3->4
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: -1->5->3->4->0
+ * Output: -1->0->3->4->5
+ * 
+ * 
+ */
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* insertionSortList(ListNode* head) {
+        if (!head || !head->next) {
+            return head;
+        }
+        ListNode *cur = head->next;
+        ListNode *pre_cur = head;
+        while (cur) {
+            ListNode *next = cur->next;
+            ListNode *cmp = head;
+            ListNode *pre_cmp = NULL;
+            while (cmp != cur && cur->val > cmp->val) {
+                if (pre_cmp) {
+                    pre_cmp = pre_cmp->next;
+                } else {
+                    pre_cmp = head;
+                }
+                cmp = cmp->next;
+            }
+
+            if (cmp != cur) {
+                if (pre_cmp) {
+                    pre_cmp->next = cur;
+                } else {
+                    head = cur;
+                }
+                pre_cur->next = cur->next;
+                cur->next = cmp;
+            } else {
+                pre_cur = cur;
+            }
+            cur = next;
+        }
+        return head;
+    }
+};
+static int x=[](){
+    std::ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    return 0;
+}();

c94367adff62a3f309a0deba2930e08194500828
dodouwang <37562495+dodouwang@users.noreply.github.com>
2018-08-17 16:20:15 +0800

    update 160.intersection-of-two-linked-lists.cpp

--- 160.intersection-of-two-linked-lists.cpp
+++ 160.intersection-of-two-linked-lists.cpp
@@ -45,7 +45,15 @@
  */
 class Solution {
 public:
-    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+    ListNode *cur1 = headA, *cur2 = headB;
+    while(cur1 != cur2){
+        cur1 = cur1?cur1->next:headB;
+        cur2 = cur2?cur2->next:headA;
+    }
+    return cur1;
+}
+    ListNode *getIntersectionNode1(ListNode *headA, ListNode *headB) {
         if (!headA || !headB) return NULL;
 
         ListNode *p1 = headA;
